2cdb9a7db1143f848c2f8c1c08b4662fab5923b7
md4657800-svg <md4657800@gmail.com>
2026-07-09 02:41:53 +0400

    Update device.mk for A/B support and dynamic partitions

--- device.mk
+++ device.mk
@@ -5,31 +5,39 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/ravoz/RAVOZ_Z6_Pro
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+LOCAL_PATH := $(call my-dir)
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+# A/B Support
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+    system \
+    vendor \
+    product \
+    system_ext
 
+# Dynamic partitions for A/B (Android 11 Proper Support)
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_OTA_DYNAMIC_PARTITIONS := true
+PRODUCT_OTA_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+    product \
+    system \
+    system_ext \
+    vendor
+
+# Boot control HAL - Android 11 compatible
 PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service \
     bootctrl.mt6765
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6765 \
-    libgptutils \
-    libz \
-    libcutils
-
+# OTA packages
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# Verified Boot
+PRODUCT_SUPPORTS_VERITY := true
+PRODUCT_SUPPORTS_VERITY_FEC := true
